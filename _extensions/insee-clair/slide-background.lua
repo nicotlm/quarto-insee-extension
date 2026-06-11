90db1e5503bd3e5c3e8f3cb8e33de0840a7812be
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-06-11 11:50:10 +0000

    with page finale background in lua filter

--- _extensions/insee-clair/slide-background.lua
+++ _extensions/insee-clair/slide-background.lua
@@ -5,6 +5,13 @@ local n = 0
 local base = "_extensions/insee-clair/ressources/revealJs/2_Template-INSEE-Clair_Intercalaire"
 
 function Header(h)
+  -- Background page finale
+  if h.identifier == "pageDeFin" then
+    h.attributes["background-image"] = "_extensions/insee-clair/ressources/revealJs/6_Template-INSEE-Clair_Diapo-finale.svg"
+    h.attributes["background-size"]  = "100% 100%"
+    return h
+  end
+  -- Slides de titres de parties
   if h.level == 1 and not h.classes:includes("unnumbered") then
     n = n + 1
     local k = (n - 1) % 4 + 1                       -- 1,2,3,4,1,2,3,4 …
@@ -15,3 +22,4 @@ function Header(h)
     return h
   end
 end
+
